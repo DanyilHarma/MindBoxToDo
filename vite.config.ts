02b838070ed4c5e91dd97036c25d8f802bd32137
Danyil <danilagarma1999@mail.ru>
2024-11-13 16:45:01 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: ''
-})
+  base: '',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`
+      }
+    }
+  }
+});
