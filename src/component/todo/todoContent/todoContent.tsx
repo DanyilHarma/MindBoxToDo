4b64e520f5b76ac9aeb87a91fec2d20f9622798d
Danyil <danilagarma1999@mail.ru>
2024-11-13 13:10:51 +0200

    Added sections for each category

--- src/component/todo/todoContent/todoContent.tsx
+++ src/component/todo/todoContent/todoContent.tsx
@@ -3,18 +3,25 @@ import classes from "./todoContent.module.css"
 import AddTask from "./addTask/addTask";
 import { useTodoState } from "../../../hooks/todoState";
 import { useState } from "react";
+import CategoriesButton from "./categoriesButton/categoriesButton";
 
 const TodoContent: React.FC = () => {
     const { tasks, toggleCompleted, addTask } = useTodoState();
     const [isPopup, setIsPopup] = useState(false);
+    const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
 
     const handleAddTask = (text: string, category: string, importance: boolean) => {
         addTask(text, category, importance)
     };
 
+    const filteredTask = selectedCategory ?
+        tasks.filter(task => task.category === selectedCategory)
+        : tasks;
+
     return (
         <div className={classes.todoContent}>
-            <TasksList tasks={tasks} setCompleted={toggleCompleted} />
+            <CategoriesButton setSelectedCategory={setSelectedCategory} />
+            <TasksList tasks={filteredTask} setCompleted={toggleCompleted} />
             <div className={classes.openPopup} onClick={() => setIsPopup(true)}>
                 Add Task
             </div>
